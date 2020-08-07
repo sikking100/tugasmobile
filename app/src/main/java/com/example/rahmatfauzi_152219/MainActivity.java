3b21b5199cf5f25e40bed3221f8172dc38d861de
sikking100 <sigithadiprawira@gmail.com>
2020-08-07 16:29:28 +0800

    rahmat first

---
+++ app/src/main/java/com/example/rahmatfauzi_152219/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.rahmatfauzi_152219;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txtNama, txtPass;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtNama = (EditText) findViewById(R.id.txt_user);
+        txtPass = (EditText) findViewById(R.id.txt_pass);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (txtNama.getText().toString().equalsIgnoreCase("Rahmat Fauzi") && txtPass.getText().toString().equalsIgnoreCase("152219")) {
+                    Toast.makeText(getBaseContext(), "Berhasil Login", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Toast.makeText(getBaseContext(), "Nama pengguna atau kata sandi tidak sesuai", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
