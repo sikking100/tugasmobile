a2d8af346e9385ce4107823689ea40d782a97f54
sikking100 <sigithadiprawira@gmail.com>
2020-08-23 00:19:01 +0800

    tugas 3

--- app/src/main/java/com/example/dasrialisainung_152214/FrmDaftar.java
+++ app/src/main/java/com/example/dasrialisainung_152214/FrmDaftar.java
@@ -11,12 +11,18 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class FrmDaftar extends AppCompatActivity {
 
     EditText txtNim, txtNama, txtEmail, txtTelp;
     RadioGroup _jenkel;
     CheckBox cbBaca, cbMakan, cbTravel;
-    Button btnSave, btnClear;
+    Button btnSave, btnClear, btnView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +39,7 @@ public class FrmDaftar extends AppCompatActivity {
         cbTravel = (CheckBox) findViewById(R.id.travel);
         btnSave = (Button) findViewById(R.id.btn_save);
         btnClear = (Button) findViewById(R.id.btn_clear);
+        btnView = (Button) findViewById(R.id.btn_view);
 
         btnClear.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,7 +65,38 @@ public class FrmDaftar extends AppCompatActivity {
                 cek += check(cbMakan);
                 cek += check(cbTravel);
                 String text = "Nim: " + txtNim.getText().toString() + "\nNama: " + txtNama.getText().toString() + "\nTelepon: " + txtTelp.getText().toString() + "\nEmail: " + txtEmail.getText().toString() + "\nJenis Kelamin: " + jenkel.getText().toString() + "\nHobby: " + cek;
-                Toast.makeText(getBaseContext(), text, Toast.LENGTH_LONG).show();
+                try {
+                    FileOutputStream fileOutputStream = openFileOutput("DasrialiSainung_152214.txt", MODE_PRIVATE);
+                    OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+                    outputStreamWriter.write(text);
+                    outputStreamWriter.flush();
+                    outputStreamWriter.close();
+                    Toast.makeText(getBaseContext(), "Sukses menyimpan", Toast.LENGTH_LONG).show();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                    Toast.makeText(getBaseContext(), "Gagal menyimpan", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    FileInputStream fileInputStream = openFileInput("DasrialiSainung_152214.txt");
+                    InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+                    char[] inputBuffer = new char[100];
+                    String s = "";
+                    int charRead;
+                    while ((charRead = inputStreamReader.read(inputBuffer)) > 0) {
+                        String readString = String.copyValueOf(inputBuffer,0, charRead);
+                        s+=readString;
+                        inputBuffer = new char[100];
+                    }
+                    Toast.makeText(getBaseContext(), s, Toast.LENGTH_LONG).show();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
             }
         });
     }
